f022e0a3cc280ce958b12cf2fa2540ec91179561
223n <tatami@cmail.plala.or.jp>
2017-07-09 01:13:19 +0900

    Create Project

---
+++ src/j223nTools/j223nTools/Forms/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace j223nTools.Forms
+{
+    /// <summary>
+    /// MainForm
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeEventHandler();
+        }
+
+        /// <summary>
+        /// Initialize Event Handler
+        /// </summary>
+        private void InitializeEventHandler()
+        {
+            aboudInfoToolStripMenuItem.Click += AboudInfoToolStripMenuItem_Click;
+        }
+
+        #endregion
+
+        #region Event
+
+        private void AboudInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var f = new AboutForm()) { f.ShowDialog(this); }
+        }
+
+        #endregion
+
+    }
+}
