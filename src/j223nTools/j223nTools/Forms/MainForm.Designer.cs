7297cc7dab374e60ee8de0fd981b190c37ee4e1f
223n <tatami@cmail.plala.or.jp>
2017-07-09 12:55:32 +0900

    NICのアダプター情報をリストに表示する処理を追加 NicSettingに最新情報を反映するように設定を追加

--- src/j223nTools/j223nTools/Forms/MainForm.Designer.cs
+++ src/j223nTools/j223nTools/Forms/MainForm.Designer.cs
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.components = new System.ComponentModel.Container();
+            this.mainGrid = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,21 +43,37 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboudInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNetworkInterfaceType = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.colIPv4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIPv4DHCP = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.mainBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mainGrid)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // mainGrid
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(472, 221);
-            this.dataGridView1.TabIndex = 0;
+            this.mainGrid.AllowUserToAddRows = false;
+            this.mainGrid.AllowUserToDeleteRows = false;
+            this.mainGrid.AutoGenerateColumns = false;
+            this.mainGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.mainGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colName,
+            this.colNetworkInterfaceType,
+            this.colIPv4,
+            this.colIPv4DHCP});
+            this.mainGrid.DataSource = this.mainBindingSource;
+            this.mainGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainGrid.Location = new System.Drawing.Point(0, 24);
+            this.mainGrid.Name = "mainGrid";
+            this.mainGrid.RowTemplate.Height = 21;
+            this.mainGrid.Size = new System.Drawing.Size(472, 221);
+            this.mainGrid.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -81,7 +98,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
             this.exitToolStripMenuItem.Text = "終了(&X)";
             // 
             // InfoToolStripMenuItem
@@ -153,21 +170,62 @@
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
+            // colId
+            // 
+            this.colId.DataPropertyName = "Id";
+            this.colId.HeaderText = "Id";
+            this.colId.Name = "colId";
+            this.colId.ReadOnly = true;
+            this.colId.Width = 39;
+            // 
+            // colName
+            // 
+            this.colName.DataPropertyName = "Name";
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            this.colName.ReadOnly = true;
+            this.colName.Width = 59;
+            // 
+            // colNetworkInterfaceType
+            // 
+            this.colNetworkInterfaceType.DataPropertyName = "NetworkInterfaceType";
+            this.colNetworkInterfaceType.HeaderText = "NetworkInterfaceType";
+            this.colNetworkInterfaceType.Name = "colNetworkInterfaceType";
+            this.colNetworkInterfaceType.ReadOnly = true;
+            this.colNetworkInterfaceType.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colNetworkInterfaceType.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.colNetworkInterfaceType.Width = 142;
+            // 
+            // colIPv4
+            // 
+            this.colIPv4.DataPropertyName = "IPv4Address";
+            this.colIPv4.HeaderText = "IPv4";
+            this.colIPv4.Name = "colIPv4";
+            this.colIPv4.Width = 52;
+            // 
+            // colIPv4DHCP
+            // 
+            this.colIPv4DHCP.DataPropertyName = "IPv4IsDHCP";
+            this.colIPv4DHCP.HeaderText = "IPv4 DHCP";
+            this.colIPv4DHCP.Name = "colIPv4DHCP";
+            this.colIPv4DHCP.Width = 68;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 267);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mainGrid);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "223nTools";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainGrid)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,7 +233,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView mainGrid;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
@@ -189,6 +247,12 @@
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboudInfoToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn colNetworkInterfaceType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIPv4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colIPv4DHCP;
+        private System.Windows.Forms.BindingSource mainBindingSource;
     }
 }
 
--- src/j223nTools/j223nTools/Forms/MainForm.cs
+++ src/j223nTools/j223nTools/Forms/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace j223nTools.Forms
 {
@@ -16,7 +17,16 @@ namespace j223nTools.Forms
     public partial class MainForm : Form
     {
 
-        #region Constructor
+        #region Property
+
+        /// <summary>
+        /// NicSettings
+        /// </summary>
+        public List<Network.NicSetting> NicSettings { get; set; } = new List<Network.NicSetting>();
+
+        #endregion
+
+        #region Initialize
 
         /// <summary>
         /// Constructor
@@ -25,6 +35,7 @@ namespace j223nTools.Forms
         {
             InitializeComponent();
             InitializeEventHandler();
+            InitializeGridView();
         }
 
         /// <summary>
@@ -32,16 +43,108 @@ namespace j223nTools.Forms
         /// </summary>
         private void InitializeEventHandler()
         {
+            loadInfoToolStripMenuItem.Click += LoadInfoToolStripMenuItem_Click;
             aboudInfoToolStripMenuItem.Click += AboudInfoToolStripMenuItem_Click;
+        }
+
+        /// <summary>
+        /// Initialize GridView
+        /// </summary>
+        private void InitializeGridView()
+        {
+            InitializeColNetworkInterfaceType();
+            InitializeBindingSource();
+        }
+
+        /// <summary>
+        /// Initialize colNetworkInterfaceType
+        /// </summary>
+        private void InitializeColNetworkInterfaceType()
+        {
+            var dt = new DataTable("NetworkInterfaceTypeTable");
+            dt.Columns.Add("key", typeof(int));
+            dt.Columns.Add("value", typeof(string));
+            foreach (NetworkInterfaceType val in Enum.GetValues(typeof(NetworkInterfaceType)))
+            {
+                var name = Enum.GetName(typeof(NetworkInterfaceType), val);
+                dt.Rows.Add(val, name);
+            }
+            colNetworkInterfaceType.DisplayMember = "value";
+            colNetworkInterfaceType.ValueMember = "key";
+            colNetworkInterfaceType.DataSource = dt;
+        }
+
+        /// <summary>
+        /// Initialize GridView Datasource
+        /// </summary>
+        private void InitializeBindingSource()
+        {
+            mainBindingSource.DataSource = NicSettings;
+            mainBindingSource.ResetBindings(false);
         }
 
         #endregion
 
         #region Event
 
-        private void AboudInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Click AboutInfo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AboudInfoToolStripMenuItem_Click(object sender, EventArgs e) { using (var f = new AboutForm()) { f.ShowDialog(this); } }
+
+        /// <summary>
+        /// Click LoadNetworkInfo
+        /// </summary>
+        private void LoadInfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            using (var f = new AboutForm()) { f.ShowDialog(this); }
+            LoadNicSettings();
+            mainBindingSource.ResetBindings(false);
+        }
+
+        #endregion
+
+        #region Method
+
+        /// <summary>
+        /// Load Network Info
+        /// </summary>
+        private void LoadNicSettings()
+        {
+            var nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var adapter in nics)
+            {
+                var setting = NicSettings.Where(s => s.Id == adapter.Id).FirstOrDefault();
+                if (setting == null)
+                {
+                    setting = new Network.NicSetting();
+                    NicSettings.Add(setting);
+                }
+                setting.Id = adapter.Id;
+                setting.Name = adapter.Name;
+                setting.NetworkInterfaceType = (int)adapter.NetworkInterfaceType;
+                setting.IPv4Address = string.Empty;
+                setting.IPv4IsDHCP = false;
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    var adapterProperties = adapter.GetIPProperties();
+                    var ipv4Properties = adapterProperties.GetIPv4Properties();
+                    if (ipv4Properties == null)
+                        setting.IPv4Address = "No IPv4 Information.";
+                    else
+                    {
+                        setting.IPv4IsDHCP = ipv4Properties.IsDhcpEnabled;
+                        foreach (var ip in adapterProperties.UnicastAddresses.Where(ip => ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork))
+                        {
+                            setting.IPv4Address = ip.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+                else
+                    setting.IPv4Address = "IPv4 Unsupport Adapter.";
+            }
         }
 
         #endregion
